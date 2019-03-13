bdff8a9d1682de4dd74c235f58321b1b66f010e1
OverSwift <kabal2015@gmail.com>
2019-03-13 12:30:31 +0200

    Project structure refactoring. Add mac os app.

---
+++ YOLOv3-CoreML/ModelImageCropper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ModelImageCropper
+//
+//  Created by Sergiy Loza on 3/13/19.
+//  Copyright © 2019 MachineThink. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ YOLOv3-CoreML/ModelImageCropper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ModelImageCropper
+//
+//  Created by Sergiy Loza on 3/13/19.
+//  Copyright © 2019 MachineThink. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
