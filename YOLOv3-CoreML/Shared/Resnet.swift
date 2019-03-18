239c5e8714e2952afbef93c56676be376a801a78
OverSwift <kabal2015@gmail.com>
2019-03-18 14:41:51 +0200

    Fix typo

---
+++ YOLOv3-CoreML/Shared/Resnet.swift
@@ -0,0 +1,17 @@
+//
+//  Resnet.swift
+//  YOLOv3-CoreML
+//
+//  Created by Sergiy Loza on 3/18/19.
+//  Copyright © 2019 Lemberg Solutions. All rights reserved.
+//
+
+import Foundation
+
+class Resnet {
+    
+    public static let inputWidth = 64
+    public static let inputHeight = 64
+    
+    let resnet = resnet50()
+}
